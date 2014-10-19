0e751daae02e0ab5ea52fc9d079618c4a806acbb
Laurenz <laurenztino@yahoo.com>
2014-10-19 11:05:52 +0800

    Updating main.class

---
+++ src/preprocess/Main.java
@@ -0,0 +1,26 @@
+package preprocess;
+
+import shortcuts.Print;
+
+public class Main 
+{
+	static Print p = new Print();
+	public static void main(String[] args)
+	{
+		/* if you are looking for CoreNLP code, it has been transfered to Nlp.java 
+		 * Please keep this class as abstracted and as high level as possible :) 
+		 *  
+		 * Directives: 
+		 * 1) Call GUI 
+		 * 2) Input from GUI will be transferred to Nlp.java 
+		 * 3) Nlp.java output will be transferred to Jmwe.java
+		 * 4) Finish processing and finishing touches to input for the delight of other developers.
+		 *
+		 */
+		
+		GUI_Start.main(args);
+		ReaderWrite rw = new ReaderWrite("filepath");
+		Nlp nlp = new Nlp();
+		
+	}
+}
